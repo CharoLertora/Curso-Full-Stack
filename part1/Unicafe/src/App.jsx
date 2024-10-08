cc45ebde25024652aa4822685ee2b091062b0d46
CharoLertora <clertora@fi.uba.ar>
2024-10-08 15:57:49 -0300

    Modificaciones necesarias para el ejercicio 1.9: Unicafe

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -21,6 +21,42 @@ const Button = (props) => (
   </button>
 )
 
+const add = (num1, num2, num3) => {
+  console.log("Add:",num1,num2,num3)
+  return num1 + num2 + num3
+}
+
+const average = (num1, num2, num3) => {
+  console.log("Average:",num1,num2,num3)
+  return (num1 - num3)/add(num1,num2,num3)
+}
+
+const promPositive = (num1, num2, num3) => {
+  console.log("promPositive:",num1,num2,num3)
+  return (num1*100)/add(num1,num2,num3)
+}
+
+const Statistics = (props) => {
+  
+  if (add(props.good,props.neutral,props.bad) == 0) {
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Display value="Good: " count={props.good}/>
+      <Display value="Neutral: " count={props.neutral}/>
+      <Display value="Bad: " count={props.bad}/>
+    
+      <Display value="All: " count={add(props.good, props.neutral, props.bad)} />
+      <Display value="Average: " count={average(props.good, props.neutral, props.bad)} />
+      <Display value="Positive: " count={promPositive(props.good, props.neutral, props.bad) + "%"}/>
+    </div>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -41,13 +77,8 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
         
       <Subtitle value="Statistics" />
-      <Display value="Good:" count={good}/>
-      <Display value="Neutral:" count={neutral}/>
-      <Display value="Bad:" count={bad}/>
-
-      <Display value="All:" count={good + neutral + bad} />
-      <Display value="Average: " count={(good - bad)/(good + neutral + bad)} />
-      <Display value="Positive: " count={(good*100)/(good+neutral+bad)} />
+      <Statistics good={good} neutral={neutral} bad={bad}  />
+      
     </div>
   )
 }
