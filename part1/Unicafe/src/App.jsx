3f425d51130dcb6ec1ec660ead554b05cbeb7448
CharoLertora <clertora@fi.uba.ar>
2024-10-08 12:18:55 -0300

    Agrego cambios para ejercicio 1.6: Unicafe

---
+++ part1/Unicafe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Display = props => <div>{props.value} {props.count}</div>
+
+const Title = (props) => {
+  console.log(props)  
+  return (
+  <div>
+    <h1>
+      {props.title}
+    </h1>
+  </div>
+  )
+}
+
+const Subtitle = props => <div><h2>{props.value}</h2></div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  console.log("Value Good", good)
+  console.log("Value Neutral", neutral)
+  console.log("Value Bad", bad)
+  
+  return (
+    <div>
+      <Title title="Give your opinion" />
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+        
+      <Subtitle value="Statistics" />
+      <Display value="Good:" count={good}/>
+      <Display value="Neutral:" count={neutral}/>
+      <Display value="Bad:" count={bad}/>
+    </div>  
+  )
+}
+
+export default App
